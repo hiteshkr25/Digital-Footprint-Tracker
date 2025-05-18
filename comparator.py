bdb6015a5ac3229dd20e43c6f7ec04f80e16fdbb
hiteshkr25 <157395524+hiteshkr25@users.noreply.github.com>
2025-05-18 16:09:16 +0530

    Update comparator.py

--- comparator.py
+++ comparator.py
@@ -1,23 +1,23 @@
 import os
-from image_handler import generate_hash
+from image_handler import get_sha256
 
-REFERENCE_FOLDER = 'static/reference_images'
-os.makedirs(REFERENCE_FOLDER, exist_ok=True)
+def find_duplicate_images(target_hash, root_folder, uploaded_path):
+    match_paths = []
+    target_size = os.path.getsize(uploaded_path)
 
-def compare_with_reference_images(uploaded_hash):
-    """Compare uploaded image hash with reference images and return similarity scores."""
-    results = []
-    print("Comparing uploaded image with reference images...")
+    for root, dirs, files in os.walk(root_folder):
+        for name in files:
+            if not name.lower().endswith(('.png', '.jpg', '.jpeg')):
+                continue
 
-    for filename in os.listdir(REFERENCE_FOLDER):
-        if filename.lower().endswith(('jpg', 'jpeg', 'png')):
-            ref_path = os.path.join(REFERENCE_FOLDER, filename)
+            filepath = os.path.join(root, name)
+
             try:
-                ref_hash = generate_hash(ref_path)
-                hamming_distance = uploaded_hash - ref_hash
-                similarity_score = 100 - (hamming_distance / 64 * 100)
-                results.append((filename, round(similarity_score, 2)))
-            except Exception as e:
-                print(f"Error processing {filename}: {e}")
+                if os.path.getsize(filepath) != target_size:
+                    continue
+                if get_sha256(filepath) == target_hash:
+                    match_paths.append(filepath)
+            except Exception:
+                continue
 
-    return results
+    return match_paths
